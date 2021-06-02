61d9719abb83feb73336550a864dd6945d4dd4c0
alumno <alumno@192.168.56.1>
2021-06-02 19:20:32 +0200

    Ejercicio 2 resuelto(texto formateado)

--- src/fp/daw/exprog20210602/ejercicio2/PruebaFrecuencia.java
+++ src/fp/daw/exprog20210602/ejercicio2/PruebaFrecuencia.java
@@ -5,10 +5,10 @@ import java.io.File;
 public class PruebaFrecuencia {
 
 	public static void main(String[] args) {
-		Frecuencia quijote= new Frecuencia(new File("El Quijote ISO-8859-1.txt"));
-		System.out.println("La palabra DE, aparece: "+ quijote.veces("de")+ " veces");
-		System.out.println("La palabra MAÑANA, aparece: "+ quijote.veces("mañana")+ " veces");
-		System.out.println("La palabra P, aparece: "+ quijote.veces("p")+ " veces");
+		Frecuencia quijote = new Frecuencia(new File("El Quijote ISO-8859-1.txt"));
+		System.out.println("La palabra DE, aparece: " + quijote.veces("de") + " veces");
+		System.out.println("La palabra MAÑANA, aparece: " + quijote.veces("mañana") + " veces");
+		System.out.println("La palabra P, aparece: " + quijote.veces("p") + " veces");
 	}
 
 }
